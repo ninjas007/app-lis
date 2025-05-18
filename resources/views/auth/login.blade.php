d131a4e44ab6c0cd9ddc1c5038ae48049f6d67ae
adi <tiliztiadi@gmail.com>
2025-05-18 17:08:06 +0700

    chore: fix style form login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,7 +16,7 @@
             <div class="row justify-content-center h-100">
                 <div class="col-xl-5">
                     <div class="form-input-content">
-                        <div class="card login-form mb-0">
+                        <div class="card login-form mb-0" style="opacity: 0.85">
                             <div class="card-body">
                                 <div class="text-center">
                                     <img src="{{ asset('images/gambar-login.png') }}" alt="" style="width: 100px; height: 100px; background-size: contain; border-radius: 20px">
@@ -26,21 +26,21 @@
                                 </div>
                                 <form class="mt-2 mb-3 login-input" method="POST" action="{{ route('login') }}">
                                     @csrf
-                                    <div class="form-group mb-1">
-                                        <input type="text" class="form-control" name="username" placeholder="Username">
+                                    <div class="form-group mb-2">
+                                        <input type="text" class="form-control" name="username" placeholder="Username" style="border-bottom: 0.5px solid #a2a2a2">
 
                                         @error('username')
                                             <span class="text-danger">{{ $message }}</span>
                                         @enderror
                                     </div>
                                     <div class="form-group">
-                                        <input type="password" class="form-control" name="password" placeholder="Password">
+                                        <input type="password" class="form-control" name="password" placeholder="Password" style="border-bottom: 0.5px solid #a2a2a2">
 
                                         @error('password')
                                             <span class="text-danger">{{ $message }}</span>
                                         @enderror
                                     </div>
-                                    <button type="submit" class="btn login-form__btn btn-sm submit w-100">
+                                    <button type="submit" class="btn login-form__btn submit w-100">
                                        <i class="fa fa-sign-in"></i> Sign In
                                     </button>
                                 </form>
