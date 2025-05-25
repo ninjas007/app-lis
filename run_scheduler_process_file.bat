51c5821798ac8e88241fbeb96e64cf6ed17d0280
adi <tiliztiadi@gmail.com>
2025-05-25 15:45:37 +0700

    fix: name artisan

--- run_scheduler_process_file.bat
+++ run_scheduler_process_file.bat
@@ -4,6 +4,6 @@ cd /d "C:\laragon\www\app-lis"
 set PATH=C:\laragon\bin\php\php-8.2.19-Win32-vs16-x64;%PATH%
 echo Menjalankan HL7 Process Scheduler...
 :loop
-php artisan schedule:hl7-process
+php artisan hl7:process
 timeout /t 15 >nul
 goto loop
