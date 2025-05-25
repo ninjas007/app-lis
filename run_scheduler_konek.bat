b314d8e42ae84f6614670de47e977a71878ce39e
adi <tiliztiadi@gmail.com>
2025-05-25 15:41:56 +0700

    fix: scheduler

--- run_scheduler_konek.bat
+++ run_scheduler_konek.bat
@@ -5,5 +5,5 @@ set PATH=C:\laragon\bin\php\php-8.2.19-Win32-vs16-x64;%PATH%
 echo Menjalankan HL7 Multi-Listen Scheduler...
 :loop
 php artisan schedule:hl7-multi-listen
-timeout /t 60 >nul
+timeout /t 15 >nul
 goto loop
